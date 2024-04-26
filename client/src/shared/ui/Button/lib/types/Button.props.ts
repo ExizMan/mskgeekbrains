bb7c4236f3ffde7badd3dd15d10dfa1db6d4b7a8
Anton Zhuravlev <abramoff.job@gmail.com>
2024-04-26 22:06:31 +0300

    init frontend

---
+++ client/src/shared/ui/Button/lib/types/Button.props.ts
@@ -0,0 +1,20 @@
+import { ButtonHTMLAttributes, DetailedHTMLProps } from 'react';
+import { BorderEnum, ColorEnum, FontFamilyEnum, SizeEnum, WeightEnum } from '@shared/lib';
+
+
+export enum ButtonTypeEnum {
+    FILLED = 'filled',
+    DEFAULT = 'default',
+    DASHED = 'dashed',
+}
+
+
+export interface IButtonProps extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
+    color?: ColorEnum;
+    bgColor?: ColorEnum;
+    size?: SizeEnum;
+    fontFamily?: FontFamilyEnum;
+    weight?: WeightEnum;
+    buttonType?: ButtonTypeEnum;
+    border?: BorderEnum;
+}
