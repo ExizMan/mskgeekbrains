fc19cc49646d28aa2102f6684f9890f9afc2dd46
Yuriy Sankin <36051254+ExizMan@users.noreply.github.com>
2024-04-27 16:23:03 +0300

    Add files via upload

--- back/bot/serializers.py
+++ back/bot/serializers.py
@@ -5,18 +5,25 @@ from .models import *
 
 
 class MessageSerializer(ModelSerializer):
-    user_id = serializers.IntegerField(source='tg_id')
+
     class Meta:
         model = Message
         fields = '__all__'
 
 
 class ChatSerializer(ModelSerializer):
+    last_massage = serializers.SerializerMethodField(read_only=True)
+    username = serializers.SerializerMethodField(read_only=True)
 
 
     class Meta:
         model = Chat
         fields = '__all__'
+    def get_last_massage(self, obj):
+        return Message.objects.filter(chat_id=obj.id).last().text
+
+    def get_username(self, obj):
+        return Student.objects.get(usertg_id=obj.id).username
 
 
 class StudentSerializer(ModelSerializer):
